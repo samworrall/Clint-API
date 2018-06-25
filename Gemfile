8e6fa6453087335d2b80d6222e304062629f9b6d
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-25 14:49:56 +0100

    Set up Mongoid

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 
 source 'https://rubygems.org'
 
+gem 'mongoid'
 gem 'rubocop'
 gem 'rspec'
 gem 'sinatra'
--- server.rb
+++ server.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'mongoid'
+
+Mongoid.load! "mongoid.config"
 
 get '/' do
   'Welcome to Clint!'
