219147e3096728e9533eecab429ab560e70aaa35
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-27 14:29:23 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
-Event.create(
-  title: 'Teapot painting',description: 'Painting teapots',price: 10,
-  )
+Event.create([
+  {title: 'Teapot painting',description: 'Painting teapots',price: 10},
+  {title: 'Rock climbing',description: 'Climbing rocks',price: 15}
+  ])
 
-Availability.create(
-  date: '2018-07-01'
-)
+Availability.create([
+  {date: '2018-07-01'},
+  {date: '2018-07-02'}
+])
 
-AvailabilitiesEvent.create(
-  availability_id: 1,event_id: 1
-)
+AvailabilitiesEvent.create([
+  {availability_id: 1,event_id: 1},
+  {availability_id: 2,event_id: 2}
+])
