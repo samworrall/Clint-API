9a83e2376cd186258e08b3ed48bcac2656071991
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-27 11:56:08 +0100

    Return only 1 event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -7,14 +7,23 @@ RSpec.describe Event, type: :model do
       @availability2 = FactoryBot.create(:availability, id: 2, date: '2018-06-02')
       @event1 = FactoryBot.create(:event, id: 1)
       @event2 = FactoryBot.create(:event, id: 2)
+      @event3 = FactoryBot.create(:event, id: 3)
       @AE1 = FactoryBot.create(:availabilities_event, id: 1)
       @AE2 = FactoryBot.create(:availabilities_event, id: 2, event_id: 2)
+      @AE3 = FactoryBot.create(:availabilities_event, id: 3, event_id: 3, availability_id: 2)
     end
 
     context 'When given date is 2018-06-01' do
-      it 'Returns event1' do
+      it 'Returns event1 and event 2' do
         search_hash = { date: '2018-06-01' }
         expect(Event.search(search_hash)).to eq([@event1, @event2])
+      end
+    end
+
+    context 'When given date is 2018-06-02' do
+      it 'Returns event3 only' do
+        search_hash = { date: '2018-06-02' }
+        expect(Event.search(search_hash)).to eq([@event3])
       end
     end
   end
