142a9f14858bbc101076f630fc91a9b667b4609b
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-27 14:03:21 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
 Event.create(
   title: 'Teapot painting',description: 'Painting teapots',price: 10,
   )
+
+Availability.create(
+  date: '2018-07-01'
+)
+
+AvailabilitiesEvent.create(
+  availability_id: 1,event_id: 1
+)
