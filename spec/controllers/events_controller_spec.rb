04573cb77b84cb62690edce9fabcfa0d46bba514
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-27 13:44:06 +0100

    Update event controller tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,8 +2,19 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
 
+  before(:each) do
+    @availability1 = FactoryBot.create(:availability, id: 1)
+    @availability2 = FactoryBot.create(:availability, id: 2, date: '2018-06-02')
+    @event1 = FactoryBot.create(:event, id: 1)
+    @event2 = FactoryBot.create(:event, id: 2)
+    @event3 = FactoryBot.create(:event, id: 3)
+    @AE1 = FactoryBot.create(:availabilities_event, id: 1)
+    @AE2 = FactoryBot.create(:availabilities_event, id: 2, event_id: 2)
+    @AE3 = FactoryBot.create(:availabilities_event, id: 3, event_id: 3, availability_id: 2)
+  end
+
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {title: 'event', description: 'description', price: 10}
   }
 
   let(:invalid_attributes) {
@@ -14,17 +25,17 @@ RSpec.describe EventsController, type: :controller do
 
   describe "GET #index" do
     it "returns a success response" do
-      event = Event.create! valid_attributes
+      event = Event.create(valid_attributes)
       get :index, params: {}, session: valid_session
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
   describe "GET #show" do
     it "returns a success response" do
-      event = Event.create! valid_attributes
+      event = Event.create(valid_attributes)
       get :show, params: {id: event.to_param}, session: valid_session
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
@@ -32,25 +43,16 @@ RSpec.describe EventsController, type: :controller do
     context "with valid params" do
       it "creates a new Event" do
         expect {
-          post :create, params: {event: valid_attributes}, session: valid_session
+          post :create, params: {event: valid_attributes }, session: valid_session
         }.to change(Event, :count).by(1)
       end
 
       it "renders a JSON response with the new event" do
 
-        post :create, params: {event: valid_attributes}, session: valid_session
+        post :create, params: {event: {title: 'event', description: 'description', price: 10} }, session: valid_session
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json')
         expect(response.location).to eq(event_url(Event.last))
-      end
-    end
-
-    context "with invalid params" do
-      it "renders a JSON response with errors for the new event" do
-
-        post :create, params: {event: invalid_attributes}, session: valid_session
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq('application/json')
       end
     end
   end
@@ -58,31 +60,21 @@ RSpec.describe EventsController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {title: 'new event', description: 'new description', price: 20}
       }
 
       it "updates the requested event" do
-        event = Event.create! valid_attributes
-        put :update, params: {id: event.to_param, event: new_attributes}, session: valid_session
-        event.reload
-        skip("Add assertions for updated state")
+        event = Event.create(valid_attributes)
+        expect {
+          put :update, params: {id: event.to_param, event: new_attributes}, session: valid_session
+        }.to change { Event.last.price }.from(10).to(20)
       end
 
       it "renders a JSON response with the event" do
-        event = Event.create! valid_attributes
+        event = Event.create(valid_attributes)
 
         put :update, params: {id: event.to_param, event: valid_attributes}, session: valid_session
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('application/json')
-      end
-    end
-
-    context "with invalid params" do
-      it "renders a JSON response with errors for the event" do
-        event = Event.create! valid_attributes
-
-        put :update, params: {id: event.to_param, event: invalid_attributes}, session: valid_session
-        expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to eq('application/json')
       end
     end
@@ -90,7 +82,7 @@ RSpec.describe EventsController, type: :controller do
 
   describe "DELETE #destroy" do
     it "destroys the requested event" do
-      event = Event.create! valid_attributes
+      event = Event.create(valid_attributes)
       expect {
         delete :destroy, params: {id: event.to_param}, session: valid_session
       }.to change(Event, :count).by(-1)
