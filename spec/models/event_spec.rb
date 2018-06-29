f4f08226fd917734c3d90d599320d466744be6eb
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-29 14:33:05 +0100

    Begin adding seeds

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,18 +3,7 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   describe '#search' do
     before(:each) do
-      @availability1 = FactoryBot.create(:availability, id: 1)
-      @availability2 = FactoryBot.create(:availability, id: 2, date: '2018-06-02')
-      @availability3 = FactoryBot.create(:availability, id: 3, date: '2018-06-03')
-      @event1 = FactoryBot.create(:event, id: 1)
-      @event2 = FactoryBot.create(:event, id: 2, location: 'Edinburgh', price_range: 2)
-      @event3 = FactoryBot.create(:event, id: 3, price_range: 3, category: 'group fun')
-      @event4 = FactoryBot.create(:event, id: 4)
-      @AE1 = FactoryBot.create(:availabilities_event, id: 1)
-      @AE2 = FactoryBot.create(:availabilities_event, id: 2, event_id: 2)
-      @AE3 = FactoryBot.create(:availabilities_event, id: 3, event_id: 3, availability_id: 2)
-      @AE4 = FactoryBot.create(:availabilities_event, id: 4, event_id: 4, availability_id: 3)
-      @AE5 = FactoryBot.create(:availabilities_event, id: 5, event_id: 2, availability_id: 3)
+      create_factory_objects
     end
 
     context '#params_present' do
@@ -79,6 +68,13 @@ RSpec.describe Event, type: :model do
         it 'Returns event1' do
           search_hash = { date: '2018-06-03', location: 'London', price_range: 1 }
           expect(Event.search(search_hash)).to eq([@event4])
+        end
+      end
+
+      context 'When price range is 2' do
+        it 'Returns event2' do
+          search_hash = { date: '2018-06-01', location: 'Edinburgh', price_range: 2 }
+          expect(Event.search(search_hash)).to eq([@event2])
         end
       end
     end
